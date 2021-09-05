adad70eb8880367ffa151954271fb2ad3fdf0b78
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-09-05 13:54:56 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install libgit2-dev -y
+
+cd tmp
+git clone https://github.com/getsolus/solbuild.git --depth 1
+cd solbuild
+make ensure_modules
+make
+sudo make install
+
+cd $HOME
+sudo solbuild init -u
+sudo solbuild update
