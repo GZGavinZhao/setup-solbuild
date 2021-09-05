c90c326a94d5d8e2dca9940192824be1eedb7359
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-09-05 15:19:09 -0400

    Make actions prettier

--- setup.sh
+++ setup.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+echo "::group::install dependencies"
 sudo apt install libgit2-dev libcurl4-openssl-dev -y
+echo "::endgroup::"
 
+echo "::group::clone solbuild"
 cd /tmp
 git clone https://github.com/getsolus/solbuild.git --depth 1
 cd solbuild
+echo "::endgroup::"
+
+echo "::group::build solbuild"
 make ensure_modules
 make
+echo "::endgroup::"
+echo "::group::install solbuild"
 sudo make install
+echo "::endgroup::"
 
+echo "::group::init solbuild"
 cd $HOME
 sudo solbuild init -u
 sudo solbuild update
+echo "::endgroup::"
